0d8a271071f2ba6e54340f011af050b57f533619
leinadsened <84383252+leinadsened@users.noreply.github.com>
2025-04-06 05:50:25 +0000

    Deploying to gh-pages from @ leinadsened/bolondforint@5b2f0432729fe0f67a1dd421d80964e8651834ea 🚀

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Test Post",
+        "excerpt":"This is a test post.  ","categories": [],
+        "tags": [],
+        "url": "/2023/10/27/test-post.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2025/03/22/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "HUF STRIKES BACK! EUR Crumbles as Forint Gains Ground!",
+        "excerpt":"Hold on to your hats, folks! The Hungarian Forint just landed a blow on the Eurozone bully! The HUF showed its muscles, rising by a shocking -0.027544070512820512% against the EUR. Is this the beginning of the end for the Euro’s dominance? Is Hungary about to become an economic SUPERPOWER? Maybe...","categories": [],
+        "tags": [],
+        "url": "/2025/03/23/exchange-rate.html",
+        "teaser": null
+      },{
+        "title": "EUR DEMOLISHES HUF AGAIN! Will the Hungarian Currency Ever Recover?",
+        "excerpt":"BREAKING: The Euro continues its relentless assault on the Hungarian Forint! Today, the EUR surged to 402.61 HUF, a brutal 2.14 HUF increase! That’s a shocking 0.53% beatdown in a single day! Is this the END for the HUF? How much longer can Hungarian citizens withstand this economic onslaught? Yesterday’s...","categories": [],
+        "tags": [],
+        "url": "/2025/03/29/exchange-rate.html",
+        "teaser": null
+      },{
+        "title": "EURÓ VISSZAVÁG! A FORINT OSTROM ALATT, AHOGY AZ EURÓ ÚJRA EMELKEDIK!",
+        "excerpt":"A magyar forint a padlón! Egy rövid fellélegzés után az euró ismét támadásba lendült, szétzúzva a forint védelmét. Az euró jelenleg 402,89 forinton áll, ami hatalmas ugrás a tegnapi 401,9 forinthoz képest! Ez 0,99 pontos emelkedés! Brutális 0,25%-os ugrás! Vajon ez a FORINT VÉGE? Készülj fel a pénzügyi CSILLAGHULLÁSRA! Az...","categories": [],
+        "tags": [],
+        "url": "/2025/04/01/exchange-rate.html",
+        "teaser": null
+      },{
+        "title": "EURÓ SZÉTVERVE! A FORINT ÓRIÁSI GYŐZELMET ARATOTT! Ez lenne az euró vége?",
+        "excerpt":"A magyar forint kiütötte az eurót! Napokig tartó könyörtelen nyomás után a forint elszabadult, maga mögött hagyva a vergődő eurót. Az árfolyam zuhant 402,89-ről egy nyomorult 401,08-ra! Ez egy hatalmas, -1,81 pontos esés, egy sokkoló -0,45%-os összeomlás! Vajon ez a kezdete az euró dominanciájának végének? Vajon a forint folytatja majd...","categories": [],
+        "tags": [],
+        "url": "/2025/04/02/exchange-rate.html",
+        "teaser": null
+      },{
+        "title": "A FORINT SOKKOLÓ VERESÉGET SZENVEDETT! Az euró LEZÚZTA a magyar valutát – Vajon EZ A VÉG a forintnak?",
+        "excerpt":"A magyar forint tántorog az eurótól kapott újabb BRUTÁLIS pofon után! A mai árfolyamok szerint az euró letarolta a forintot, 400,62-re esett a tegnapi szánalmas 401,08-ról. Ez egy -0,46 pontos LEGYŐZÉS! Egy -0,11%-os zuhanás! Vajon ez a VÉG a forint számára? Túléli Magyarország ezt az euró-offenzívát? Lebontjuk a PUSZTÍTÓ részleteket,...","categories": [],
+        "tags": [],
+        "url": "/2025/04/03/exchange-rate.html",
+        "teaser": null
+      },{
+        "title": "AZ EURÓ SZÉTTÉPTE A FORINTOT! Vajon A MAGYAR FORINT TÚLÉLI EZT A ROHAMOT?!",
+        "excerpt":"KÉSZÜLJETEK! Az euró brutális támadást indított a forint ellen, gazdasági pusztulást hagyva maga után! Tegnap az euró még szerény 400,62 forinton állt. MA? Egy gigantikus 404,87 forinton! Ez egy 4,25 forintos UGRÁS! Egy kolosszális 1,06%-os emelkedés! Vajon ez a VÉG a magyar forint számára? Magyarország a pénzügyi csőd felé rohan?...","categories": [],
+        "tags": [],
+        "url": "/2025/04/04/exchange-rate.html",
+        "teaser": null
+      }]
