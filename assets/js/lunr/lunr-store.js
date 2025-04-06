6fd949e12426b5ef86cb9462a53711e98cf1e5dc
leinadsened <84383252+leinadsened@users.noreply.github.com>
2025-04-06 05:53:09 +0000

    Deploying to gh-pages from @ leinadsened/bolondforint@a9226a809fb76a493561ad527f1c1c22c6550752 🚀

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,28 +1,4 @@
 var store = [{
-        "title": "Test Post",
-        "excerpt":"This is a test post.  ","categories": [],
-        "tags": [],
-        "url": "/2023/10/27/test-post.html",
-        "teaser": null
-      },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/2025/03/22/welcome-to-jekyll.html",
-        "teaser": null
-      },{
-        "title": "HUF STRIKES BACK! EUR Crumbles as Forint Gains Ground!",
-        "excerpt":"Hold on to your hats, folks! The Hungarian Forint just landed a blow on the Eurozone bully! The HUF showed its muscles, rising by a shocking -0.027544070512820512% against the EUR. Is this the beginning of the end for the Euro’s dominance? Is Hungary about to become an economic SUPERPOWER? Maybe...","categories": [],
-        "tags": [],
-        "url": "/2025/03/23/exchange-rate.html",
-        "teaser": null
-      },{
-        "title": "EUR DEMOLISHES HUF AGAIN! Will the Hungarian Currency Ever Recover?",
-        "excerpt":"BREAKING: The Euro continues its relentless assault on the Hungarian Forint! Today, the EUR surged to 402.61 HUF, a brutal 2.14 HUF increase! That’s a shocking 0.53% beatdown in a single day! Is this the END for the HUF? How much longer can Hungarian citizens withstand this economic onslaught? Yesterday’s...","categories": [],
-        "tags": [],
-        "url": "/2025/03/29/exchange-rate.html",
-        "teaser": null
-      },{
         "title": "EURÓ VISSZAVÁG! A FORINT OSTROM ALATT, AHOGY AZ EURÓ ÚJRA EMELKEDIK!",
         "excerpt":"A magyar forint a padlón! Egy rövid fellélegzés után az euró ismét támadásba lendült, szétzúzva a forint védelmét. Az euró jelenleg 402,89 forinton áll, ami hatalmas ugrás a tegnapi 401,9 forinthoz képest! Ez 0,99 pontos emelkedés! Brutális 0,25%-os ugrás! Vajon ez a FORINT VÉGE? Készülj fel a pénzügyi CSILLAGHULLÁSRA! Az...","categories": [],
         "tags": [],
