7a001ffc647f3a48d6443bda45d35dd32668df83
leinadsened <84383252+leinadsened@users.noreply.github.com>
2025-04-15 11:00:39 +0000

    Deploying to gh-pages from @ leinadsened/bolondforint@f5bf5b440a0b07f2760c6cd45d5fe1eafaf71283 🚀

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -58,4 +58,10 @@ var store = [{
         "tags": [],
         "url": "/2025/04/14/exchange-rate.html",
         "teaser": null
+      },{
+        "title": "A FORINT VISSZAVÁG! Az euró MEGSZÍVTA a napi árfolyamharcban!",
+        "excerpt":"Ne tévedjünk, a valuta-csatatéren a forint MA NAGYOT RÚGOTT az euróba! A tegnapi pökhendi 410,15 LEVONULT a színről, a földbe döngölték 409,96-ra! Ez egy -0,19-es VERESÉG az euró-elitnek! Majd azt mondják, hogy a -0,05% ‘semmi’ - HAZUGSÁG! Ez a forint vére, bizonyítva, hogy nem hagyja magát lökdösni! Az euró GYENGÜL,...","categories": [],
+        "tags": [],
+        "url": "/2025/04/15/exchange-rate.html",
+        "teaser": null
       }]
