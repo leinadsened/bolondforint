cc67d20fc570c1e29c845260b4a035d58db5bbea
leinadsened <84383252+leinadsened@users.noreply.github.com>
2025-04-16 11:00:43 +0000

    Deploying to gh-pages from @ leinadsened/bolondforint@893ca12a68cb26f1e6b079e7c5edd521e1cc1ffe 🚀

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -64,4 +64,10 @@ var store = [{
         "tags": [],
         "url": "/2025/04/15/exchange-rate.html",
         "teaser": null
+      },{
+        "title": "SHOCKWAVE! Forint Magician's DECEPTIVE PLUNGE Below 409! Is Euro Detective BLIND to the Looming COLLAPSE?!",
+        "excerpt":"Hold onto your wallets! The currency battlefield just witnessed a move so cunning, it reeks of dark magic! The Forint, that slippery illusionist of the markets, just PULLED A FAST ONE, dipping from 409.96 to a shocking 408.73 against the Euro! That’s a 1.23 point drop! A so-called ‘-0.30%’ change!...","categories": [],
+        "tags": [],
+        "url": "/2025/04/16/exchange-rate.html",
+        "teaser": null
       }]
