125b70f774ef7569fa012e9d5bf8bff4b41d8eb0
leinadsened <84383252+leinadsened@users.noreply.github.com>
2025-05-02 11:00:38 +0000

    Deploying to gh-pages from @ leinadsened/bolondforint@605002605ccdf3138c3d29749f709b1c8d8390be 🚀

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -118,4 +118,10 @@ var store = [{
         "tags": [],
         "url": "/2025/05/01/exchange-rate.html",
         "teaser": null
+      },{
+        "title": "Szünetel a harc, A Munka ünnepe miatt ma pihennek az árfolyamok",
+        "excerpt":"Kedves Olvasóink! Ma, 2025. május 2-án, a Munka ünnepén sajnos szüneteltetnünk kell szokásos szatírikus tudósításunkat az euró és a forint epikus küzdelméről. Mivel ezen a munkaszüneti napon a Magyar Nemzeti Bank sem közöl friss árfolyamokat, így mi sem tudjuk követni, éppen ki áll nyerésre a devizapiacok csataterén. De ne csüggedjenek,...","categories": [],
+        "tags": [],
+        "url": "/2025/05/02/exchange-rate.html",
+        "teaser": null
       }]
